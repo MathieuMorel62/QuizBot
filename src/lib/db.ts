ba3c467245aec490265b6e964cab4e65339796a0
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-21 19:09:58 +0000

    Add comments

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import "server-only";
  
+
+// Declaration d'une variable globale pour le cache de l'instance PrismaClient
 declare global {
   // eslint-disable-next-line no-var, no-unused-vars
   var cachedPrisma: PrismaClient;
 }
 
+
+// Instance de PrismaClient
 export let prisma: PrismaClient;
+// Si l'environnement est en production, crée un nouveau client PrismaClient
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
+  // Si l'environnement est en développement, utilise l'instance PrismaClient en cache
   if (!global.cachedPrisma) {
     global.cachedPrisma = new PrismaClient();
   }
