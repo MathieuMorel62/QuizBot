07b8dfdaca2bbb806d7aa6b1477be012d9c4e899
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-21 18:59:25 +0000

    feat: Ajouter le chargement différé des images pour améliorer les temps de chargement de la page

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import { authOptions } from "@/lib/nextauth";
 import NextAuth from "next-auth/next";
 
+
+// Initialisation de la gestion de l'authentification avec NextAuth
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
