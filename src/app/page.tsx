27205b66570b27a6a34be046b019291b6faa78e8
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-21 18:59:21 +0000

    Add comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
 import SignInButton from "@/components/SignInButton";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle
-} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { redirect } from "next/navigation";
 import { getAuthSession } from "@/lib/nextauth";
 
 
+// Page d'accueil de l'application
 export default async function Home() {
+  // Vérifier si l'utilisateur est connecté
   const session = await getAuthSession();
   if (session?.user) {
     return redirect("/dashboard");
   }
 
+  // Afficher la page de connexion si l'utilisateur n'est pas connecté
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <Card className="w-[300px]">
