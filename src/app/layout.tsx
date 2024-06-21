0f34ada094cd9dd97354172e88a7d8ae06668b3f
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-21 18:59:07 +0000

    Add comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,19 +6,24 @@ import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 
 
+// Chargement de la police Inter
 const inter = Inter({ subsets: ["latin"] });
 
 
+// Définition des métadonnées de la page
 export const metadata: Metadata = {
   title: "QuizBot",
-  description: "Generated Quizzes for your students",
+  description: "Generated Quizzes for Everyone",
 };
 
+
+// Définition des propriétés de la page de base de l'application
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // Affichage de la page de base de l'application
   return (
     <html lang="en">
       <body className={cn(inter.className, "antialiased min-h-screen pt-16")}>
