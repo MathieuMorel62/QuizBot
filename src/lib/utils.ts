c6b8ce535da60c2c3e0ae6c00d1d4b7852363603
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-21 19:10:09 +0000

    Add Comments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 
+// Fonction utilitaire pour fusionner les classes CSS
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
