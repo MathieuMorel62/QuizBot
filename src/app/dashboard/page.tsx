d73e128d37dedb506fbe3bcb0429aa6c8993e6ff
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-21 19:01:12 +0000

    Add comments, HotTopicsCard and RecentActivities components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,20 +2,29 @@ import { getAuthSession } from '@/lib/nextauth'
 import { redirect } from 'next/navigation'
 import QuizMeCard from '@/components/dashboard/QuizMeCard'
 import HistoryCard from '@/components/dashboard/HistoryCard'
+import HotTopicsCard from './HotTopicsCard'
+import RecentActivities from './RecentActivities'
 import React from 'react'
 
 
 type Props = {}
 
+
+// Métadonnées de la page
 const metadata = {
   title: 'Accueil | QuizBot',
 }
 
+
+// Page d'accueil du tableau de bord
 const Dashboard = async (props: Props) => {
+  // Vérifie si l'utilisateur est connecté
   const session = await getAuthSession();
   if (!session?.user) {
     return redirect("/");
   }
+
+  // Affiche la page d'accueil du tableau de bord
   return (
     <main className='p-8 mx-auto max-w-7xl'>
       <div className='flex items-center'>
@@ -25,7 +34,10 @@ const Dashboard = async (props: Props) => {
         <QuizMeCard />
         <HistoryCard />
       </div>
-      <div className='grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7'></div>
+      <div className='grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7'>
+        <HotTopicsCard />
+        <RecentActivities />
+      </div>
     </main>
   )
 }
