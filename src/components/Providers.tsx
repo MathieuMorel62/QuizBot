d29d40f37294fb08f532e14327e0e9cfccad7f31
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-21 19:03:32 +0000

    Add comments

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import { SessionProvider } from 'next-auth/react'
 import React from 'react'
-import { ThemeProvider as NextThemesProvider, ThemeProvider } from "next-themes"
+import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 
+// Fournisseur de thème pour les composants de l'application
 const Providers = ({ children, ...props }: ThemeProviderProps) => {
+
+  // Rendu du composant Providers
   return (
     <NextThemesProvider attribute='class' defaultTheme='system' enableSystem>
       <SessionProvider>{children}</SessionProvider>
