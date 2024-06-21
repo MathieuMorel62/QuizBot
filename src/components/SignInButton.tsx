bbbbc03c6cf0b6a97f643ca4f10459884d6b26fd
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-21 19:04:30 +0000

    Add comments

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -5,11 +5,15 @@ import { Button } from './ui/button'
 import { signIn } from 'next-auth/react'
 
 
+// Propriétés du composant SignInButton
 type Props = {
   text: string
 }
 
+
+// Composant SignInButton pour la connexion
 const SignInButton = ({ text }: Props) => {
+  // Rendu du composant SignInButton
   return (
     <Button
       onClick={() => {
