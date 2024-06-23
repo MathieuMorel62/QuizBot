5ccb2c85a1d35a0bcd9834e82ad80f3da30b9f7b
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-23 18:17:12 +0000

    Update API route for creating quiz questions and add strict_output function for generating formatted outputs

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,45 +1,49 @@
-import { NextResponse } from "next/server"
-import { quizCreationSchema } from "../../schemas/form/quiz"
-import { ZodError } from "zod"
-import { strict_output } from "../../../lib/gpt"
-import { getAuthSession } from "../../../lib/nextauth"
+import { strict_output } from "@/lib/gpt";
+import { getAuthSession } from "@/lib/nextauth";
+import { getQuestionsSchema } from "../../schemas/questions";
+import { NextResponse } from "next/server";
+import { ZodError } from "zod";
 
 
-// POST /api/questions
-export const POST = async (req: Request) => {
+// Configuration de l'exécution
+export const runtime = "nodejs";
+export const maxDuration = 500;
+
+export async function POST(req: Request, res: Response) {
   try {
-    // Vérifier si l'utilisateur est connecté
-    const session = await getAuthSession()
+    // Obtenir la session d'authentification
+    const session = await getAuthSession();
+    /*
+    // Vérifier si l'utilisateur est authentifié
     if (!session?.user) {
       return NextResponse.json(
-        {
-          error: 'Vous devez être connecté pour créer un quiz',
-        },
+        { error: "Vous devez être connecté pour créer un jeu." },
         {
           status: 401,
         }
       );
     }
-
-    // Récupérer et parser le body de la requête
-    const body = await req.json()
-    const { amount, topic, type } = quizCreationSchema.parse(body)
-
+    */
+    // Récupérer et parser le corps de la requête
+    const body = await req.json();
+    const { amount, topic, type } = getQuestionsSchema.parse(body);
     let questions: any;
-
-    // Générer les questions en fonction du type de quiz
-    if (type === 'open_ended') {
+    
+    // Générer des questions ouvertes
+    if (type === "open_ended") {
       questions = await strict_output(
         "You are a helpful AI that is able to generate a pair of question and answers, the length of each answer should not be more than 15 words, store all the pairs of answers and questions in a JSON array",
         new Array(amount).fill(
-          `You are to generate a random hard open-ended questions about ${topic}`,
+          `You are to generate a random hard open-ended questions about ${topic}`
         ),
         {
           question: "question",
           answer: "answer with max length of 15 words",
         }
       );
-    } else if (type === 'mcq') {
+    } 
+    // Générer des questions à choix multiples (MCQ)
+    else if (type === "mcq") {
       questions = await strict_output(
         "You are a helpful AI that is able to generate mcq questions and answers, the length of each answer should not be more than 15 words, store all answers and questions and options in a JSON array",
         new Array(amount).fill(
@@ -54,38 +58,34 @@ export const POST = async (req: Request) => {
         }
       );
     }
-
-    // Retourner les questions générées
+    // Retourner les questions générées dans la réponse
     return NextResponse.json(
       {
-        questions,
+        questions: questions,
       },
       {
         status: 200,
       }
     );
   } catch (error) {
+    // Gestion des erreurs de validation
     if (error instanceof ZodError) {
-      // Retourner les erreurs de validation
       return NextResponse.json(
-        {
-          error: error.issues,
-        },
+        { error: error.issues },
         {
           status: 400,
         }
       );
-    } else {
-      // Retourner une erreur interne du serveur
-      console.error(error);
+    } 
+    // Gestion des erreurs inattendues
+    else {
+      console.error("Erreur GPT", error);
       return NextResponse.json(
-        {
-          error: 'Internal Server Error',
-        },
+        { error: "Une erreur inattendue s'est produite." },
         {
           status: 500,
         }
       );
     }
   }
-};
+}
